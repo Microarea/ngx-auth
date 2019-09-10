770ce9b6a190640ccb83d7220fcb4ead100dd5bb
Fabrizio De Bortoli <debortoli.fabrizio@gmail.com>
2019-09-10 11:15:54 +0200

    v1.4.0

--- lib/auth.interceptor.d.ts
+++ lib/auth.interceptor.d.ts
@@ -1,6 +1,8 @@
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { TbAuthService } from './auth.service';
 export declare class TbAuthInterceptor implements HttpInterceptor {
-    constructor();
+    private authService;
+    constructor(authService: TbAuthService);
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>;
 }
--- lib/auth.service.d.ts
+++ lib/auth.service.d.ts
@@ -1,6 +1,5 @@
 import { HttpClient, HttpBackend } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
 import { LoginRequest } from './models/login-request';
 import { LoginResponse } from './models/login-response';
 import { LogoffResponse } from './models/logoff-response';
@@ -13,15 +12,14 @@ export declare class TbAuthService {
     errorMessage: string;
     redirectUrl: string;
     constructor(env: any, http: HttpClient, handler: HttpBackend, router: Router);
-    login(loginRequest: LoginRequest): Promise<LoginResponse>;
-    isValidToken(authtoken?: any): Promise<any>;
-    getCompaniesForUser(user: string): Observable<any>;
-    getAccountName(): void;
     /**
      * Ritorna la base url del backend,
      * caricata da un file di configurazione caricato dinamicamente (assets/environment.json)
      */
     getBaseUrl(): string;
+    login(loginRequest: LoginRequest): Promise<LoginResponse>;
+    isValidToken(authtoken?: any): Promise<any>;
+    getCompaniesForUser(user: string): import("rxjs").Observable<any>;
     getIsValidTokenUrl(): string;
     getLoginUrl(): string;
     getLogoutUrl(): string;
@@ -29,7 +27,8 @@ export declare class TbAuthService {
     getResetPasswordUrl(): string;
     getSubsKeysForAccountUrl(): string;
     logoff(): Promise<LogoffResponse>;
-    saveCulture(culture?: string, uiCulture?: string): void;
     clearStorage(): void;
     private storageData;
+    getToken(): string;
+    getAccountName(): string;
 }
