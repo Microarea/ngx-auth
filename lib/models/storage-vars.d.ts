bb9e88189bc9b6a13669842464aa29cb034e0f21
paolodellepiane <paolo.dellepiane@microarea.it>
2020-05-12 09:29:34 +0200

    v1.1.2.217

--- lib/models/storage-vars.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class StorageVars {
-    static JWT: string;
-    static CULTURE: string;
-    static UI_CULTURE: string;
-    static ACCOUNT_NAME: string;
-    static ACCOUNT_ROLES: string;
-    static SUBSCRIPTION: string;
-    static SUBSCRIPTION_DESCRIPTION: string;
-}
--- lib/models/subscription.model.d.ts
+++
@@ -1,4 +0,0 @@
-export interface Subscription {
-    subscriptionkey: string;
-    description: string;
-}
